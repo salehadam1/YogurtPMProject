2a1c10a4b9bb97bd64d35fcc83ffc24fa2cc5259
RyanClinton777 <ryanmathewclinton@hotmail.com>
2020-03-04 05:25:28 +0000

    Splash Page, Visuals

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ArticleSectionComponent } from './article-section/article-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleSectionComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/article-section/article-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article-section',
+  templateUrl: './article-section.component.html',
+  styleUrls: ['./article-section.component.css']
+})
+export class ArticleSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
